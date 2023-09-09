98329fb7927aedd1b7bddd2a7ecb36704ed16724
Minin <70429604+MininxD@users.noreply.github.com>
2023-09-09 15:43:26 +0700

    Update community.js

--- src/community.js
+++ src/community.js
@@ -1,16 +1,32 @@
-/*
-insert this code with new line
-
+/*Template :
 <li onclick="copy(this)" class="youricon-icon ic-l ic-dark"><a>youricon-icon</a></li>
 */
-
 var content = `
 
 
-<li onclick="copy(this)" class="mininxd-icon ic-l ic-dark"><a>mininxd-icon</a></li>
+
+<li 
+onclick="copy(this)" 
+class="mininxd-icon ic-l ic-dark">
+<a>mininxd-icon</a>
+</li>
 
     
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
     `;
 
