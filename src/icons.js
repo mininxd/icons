8c0da9fd74ec8e3c1070f7b5d4220d18ef0b818d
Minin <70429604+MininxD@users.noreply.github.com>
2023-09-09 11:04:53 +0700

    Add files via upload

--- src/icons.js
+++ src/icons.js
@@ -1,3 +1,8 @@
 $("head").append(
   '<link rel="stylesheet" href="https://icons.mininxd.my.id/css/main.css" /><link rel="stylesheet" href="https://icons.mininxd.my.id/css/main1.css" />'
 );
+
+/*
+$("head").append(
+  '<link rel="stylesheet" href="./css/main.css" /><link rel="stylesheet" href="./css/main1.css" />'
+);*/
