257fbe1ea8b53b3ee7e79355939934c28be026ca
Minin <70429604+MininxD@users.noreply.github.com>
2023-09-09 09:17:46 +0700

    Delete content directory

--- content/os.js
+++
@@ -1,9 +0,0 @@
-var content = `
-<h3>Sistem Operasi:</h3>
-      <div onclick="copy(this)" class="manjaro-icon ic-l ic-dark">
-        manjaro-icon
-      </div>
-    
-`;
-
-document.getElementById("merk").innerHTML = content;
