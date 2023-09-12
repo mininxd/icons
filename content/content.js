f5ce5ffc8c7b0536c63a1224194e4f6e4154099e
Minin <70429604+MininxD@users.noreply.github.com>
2023-09-12 13:25:02 +0700

    Add files via upload

--- content/content.js
+++ content/content.js
@@ -1,5 +1,7 @@
-var content = `
-<li onclick="copy(this)" class="fb-icon ic-l ic-dark"><a>fb-icon</a></li>
+var content =
+  "<ul id='acak'>" +
+  `
+  <li onclick="copy(this)" class="fb-icon ic-l ic-dark"><a>fb-icon</a></li>
     <li onclick="copy(this)" class="wechat-icon ic-l ic-dark"><a>wechat-icon</a></li>
           <li onclick="copy(this)" class="messenger-icon ic-l ic-dark"><a>messenger-icon</a></li>
         <li onclick="copy(this)" class="twitter-icon ic-l ic-dark"><a>twitter-icon</a></li>
@@ -55,8 +57,141 @@ var content = `
             <li onclick="copy(this)" class="googleplay-icon ic-l ic-dark"><a>googleplay-icon</a></li>
             <li onclick="copy(this)" class="yt-icon ic-l ic-dark"><a>yt-icon</a></li>
       <li onclick="copy(this)" class="ytmusic-icon ic-l ic-dark"><a>ytmusic-icon</a></li>
-      `;
-document.getElementById("konten1").innerHTML = content;
+      <li onclick="copy(this)" class="affinitypublisher-icon ic-l ic-dark"><a>affinitypublisher-icon</a></li>
+<li onclick="copy(this)" class="affinitydesigner-icon ic-l ic-dark"><a>affinitydesigner-icon</a></li>
+<li onclick="copy(this)" class="affinityphoto-icon ic-l ic-dark"><a>affinityphoto-icon</a></li>
+<li onclick="copy(this)" class="affinity-icon ic-l ic-dark"><a>affinity-icon</a></li>
+<li onclick="copy(this)" class="acer-icon ic-l ic-dark"><a>acer-icon</a></li>
+<li onclick="copy(this)" class="amazon-icon ic-l ic-dark"><a>amazon-icon</a></li>
+<li onclick="copy(this)" class="airbnb-icon ic-l ic-dark"><a>airbnb-icon</a></li>
+
+<li onclick="copy(this)" class="fourchan-icon ic-l ic-dark"><a>fourchan-icon</a></li>
+<li onclick="copy(this)" class="threem-icon ic-l ic-dark"><a>threem-icon</a></li>
+<li onclick="copy(this)" class="lg-icon ic-l ic-dark"><a>lg-icon</a></li>
+<li onclick="copy(this)" class="airasia-icon ic-l ic-dark"><a>airasia-icon</a></li>
+<li onclick="copy(this)" class="lubuntu-icon ic-l ic-dark"><a>lubuntu-icon</a></li>
+<li onclick="copy(this)" class="linkedin-icon ic-l ic-dark"><a>linkedin-icon</a></li>
+<li onclick="copy(this)" class="litecoin-icon ic-l ic-dark"><a>litecoin-icon</a></li>
+<li onclick="copy(this)" class="liberapay-icon ic-l ic-dark"><a>liberapay-icon</a></li>
+<li onclick="copy(this)" class="lamborghini-icon ic-l ic-dark"><a>lamborghini-icon</a></li>
+
+<li onclick="copy(this)" class="laravel-icon ic-l ic-dark"><a>laravel-icon</a></li>
+<li onclick="copy(this)" class="laravelnova-icon ic-l ic-dark"><a>laravelnova-icon</a></li>
+<li onclick="copy(this)" class="leagueoflegends-icon ic-l ic-dark"><a>leagueoflegends-icon</a></li>
+<li onclick="copy(this)" class="daf-icon ic-l ic-dark"><a>daf-icon</a></li>
+<li onclick="copy(this)" class="dart-icon ic-l ic-dark"><a>dart-icon</a></li>
+<li onclick="copy(this)" class="deezer-icon ic-l ic-dark"><a>deezer-icon</a></li>
+<li onclick="copy(this)" class="spotify-icon ic-l ic-dark"><a>spotify-icon</a></li>
+
+<li onclick="copy(this)" class="ducati-icon ic-l ic-dark"><a>ducati-icon</a></li>
+<li onclick="copy(this)" class="shopify-icon ic-l ic-dark"><a>shopify-icon</a></li>
+<li onclick="copy(this)" class="shopee-icon ic-l ic-dark"><a>shopee-icon</a></li>
+<li onclick="copy(this)" class="bluetooth-icon ic-l ic-dark"><a>bluetooth-icon</a></li>
+
+<li onclick="copy(this)" class="spacex-icon ic-l ic-dark"><a>spacex-icon</a></li>
+<li onclick="copy(this)" class="msi-icon ic-l ic-dark"><a>msi-icon</a></li>
+<li onclick="copy(this)" class="reddit-icon ic-l ic-dark"><a>reddit-icon</a></li>
+<li onclick="copy(this)" class="meta-icon ic-l ic-dark"><a>meta-icon</a></li>
+<li onclick="copy(this)" class="mega-icon ic-l ic-dark"><a>mega-icon</a></li>
+<li onclick="copy(this)" class="bootstrap-icon ic-l ic-dark"><a>bootstrap-icon</a></li>
+<li onclick="copy(this)" class="rss-icon ic-l ic-dark"><a>rss-icon</a></li>
+
+
+<li onclick="copy(this)" class="kotlin-icon ic-l ic-dark"><a>kotlin-icon</a></li>
+<li onclick="copy(this)" class="osu-icon ic-l ic-dark"><a>osu-icon</a></li>
+
+<li onclick="copy(this)" class="duolingo-icon ic-l ic-dark"><a>duolingo-icon</a></li>
+<li onclick="copy(this)" class="opensourcehardware-icon ic-l ic-dark"><a>opensourcehardware-icon</a></li>
+<li onclick="copy(this)" class="ios-icon ic-l ic-dark"><a>ios-icon</a></li>
+
+<li onclick="copy(this)" class="c-icon ic-l ic-dark"><a>c-icon</a></li>
+<li onclick="copy(this)" class="cplusplus-icon ic-l ic-dark"><a>cplusplus-icon</a></li>
+
+<li onclick="copy(this)" class="cisco-icon ic-l ic-dark"><a>cisco-icon</a></li>
+<li onclick="copy(this)" class="canva-icon ic-l ic-dark"><a>canva-icon</a></li>
+<li onclick="copy(this)" class="cpanel-icon ic-l ic-dark"><a>cpanel-icon</a></li>
+
+<li onclick="copy(this)" class="centos-icon ic-l ic-dark"><a>centos-icon</a></li>
+<li onclick="copy(this)" class="linux-icon ic-l ic-dark"><a>linux-icon</a></li>
+<li onclick="copy(this)" class="kalilinux-icon ic-l ic-dark"><a>kalilinux-icon</a></li>
+<li onclick="copy(this)" class="linuxmint-icon ic-l ic-dark"><a>linuxmint-icon</a></li>
+<li onclick="copy(this)" class="archlinux-icon ic-l ic-dark"><a>archlinux-icon</a></li>
+<li onclick="copy(this)" class="artixlinux-icon ic-l ic-dark"><a>artixlinux-icon</a></li>
+<li onclick="copy(this)" class="rockylinux-icon ic-l ic-dark"><a>rockylinux-icon</a></li>
+
+<li onclick="copy(this)" class="alpinelinux-icon ic-l ic-dark"><a>alpinelinux-icon</a></li>
+<li onclick="copy(this)" class="tplink-icon ic-l ic-dark"><a>tplink-icon</a></li>
+<li onclick="copy(this)" class="fblive-icon ic-l ic-dark"><a>fblive-icon</a></li>
+<li onclick="copy(this)" class="vivaldi-icon ic-l ic-dark"><a>vivaldi-icon</a></li>
+
+<li onclick="copy(this)" class="redhatopenshift-icon ic-l ic-dark"><a>redhatopenshift-icon</a></li>
+<li onclick="copy(this)" class="redhat-icon ic-l ic-dark"><a>redhat-icon</a></li>
+<li onclick="copy(this)" class="filezilla-icon ic-l ic-dark"><a>filezilla-icon</a></li>
+
+<li onclick="copy(this)" class="rakuten-icon ic-l ic-dark"><a>rakuten-icon</a></li>
+<li onclick="copy(this)" class="keras-icon ic-l ic-dark"><a>keras-icon</a></li>
+<li onclick="copy(this)" class="oracle-icon ic-l ic-dark"><a>oracle-icon</a></li>
+<li onclick="copy(this)" class="streamlit-icon ic-l ic-dark"><a>streamlit-icon</a></li>
+
+<li onclick="copy(this)" class="kofi-icon ic-l ic-dark"><a>kofi-icon</a></li>
+<li onclick="copy(this)" class="streamlit-icon ic-l ic-dark"><a>streamlit-icon</a></li>
+<li onclick="copy(this)" class="microsoft-icon ic-l ic-dark"><a>microsoft-icon</a></li>
+<li onclick="copy(this)" class="microsoftbing-icon ic-l ic-dark"><a>microsoftbing-icon</a></li>
+<li onclick="copy(this)" class="microsoftedge-icon ic-l ic-dark"><a>microsoftedge-icon</a></li>
+<li onclick="copy(this)" class="dotnet-icon ic-l ic-dark"><a>dotnet-icon</a></li>
+<li onclick="copy(this)" class="roblox-icon ic-l ic-dark"><a>roblox-icon</a></li>
+<li onclick="copy(this)" class="logitech-icon ic-l ic-dark"><a>logitech-icon</a></li>
+<li onclick="copy(this)" class="discord-icon ic-l ic-dark"><a>discord-icon</a></li>
+<li onclick="copy(this)" class="adobe-icon ic-l ic-dark"><a>adobe-icon</a></li>
+<li onclick="copy(this)" class="acrobat-icon ic-l ic-dark"><a>acrobat-icon</a></li>
+<li onclick="copy(this)" class="audition-icon ic-l ic-dark"><a>audition-icon</a></li>
+<li onclick="copy(this)" class="dreamweaver-icon ic-l ic-dark"><a>dreamweaver-icon</a></li>
+<li onclick="copy(this)" class="illustrator-icon ic-l ic-dark"><a>illustrator-icon</a></li>
+<li onclick="copy(this)" class="creativecloud-icon ic-l ic-dark"><a>creativecloud-icon</a></li>
+<li onclick="copy(this)" class="lightroom-icon ic-l ic-dark"><a>lightroom-icon</a></li>
+<li onclick="copy(this)" class="photoshop-icon ic-l ic-dark"><a>photoshop-icon</a></li>
+<li onclick="copy(this)" class="premierepro-icon ic-l ic-dark"><a>premierepro-icon</a></li>
+
+<li onclick="copy(this)" class="indesign-icon ic-l ic-dark"><a>indesign-icon</a></li>
+
+<li onclick="copy(this)" class="adobexd-icon ic-l ic-dark"><a>adobexd-icon</a></li>
+<li onclick="copy(this)" class="adblock-icon ic-l ic-dark"><a>adblock-icon</a></li>
+<li onclick="copy(this)" class="adblockplus-icon ic-l ic-dark"><a>adblockplus-icon</a></li>
+<li onclick="copy(this)" class="adguard-icon ic-l ic-dark"><a>adguard-icon</a></li>
+<li onclick="copy(this)" class="godotengine-icon ic-l ic-dark"><a>godotengine-icon</a></li>
+<li onclick="copy(this)" class="pihole-icon ic-l ic-dark"><a>pihole-icon</a></li>
+
+<li onclick="copy(this)" class="androidstudio-icon ic-l ic-dark"><a>androidstudio-icon</a></li>
+<li onclick="copy(this)" class="android-icon ic-l ic-dark"><a>android-icon</a></li>
+<li onclick="copy(this)" class="fdroid-icon ic-l ic-dark"><a>fdroid-icon</a></li>
+<li onclick="copy(this)" class="leica-icon ic-l ic-dark"><a>leica-icon</a></li>
+<li onclick="copy(this)" class="epicgames-icon ic-l ic-dark"><a>epicgames-icon</a></li>
+
+<li onclick="copy(this)" class="origin-icon ic-l ic-dark"><a>origin-icon</a></li>
+<li onclick="copy(this)" class="alienware-icon ic-l ic-dark"><a>alienware-icon</a></li>
+<li onclick="copy(this)" class="nzxt-icon ic-l ic-dark"><a>nzxt-icon</a></li>
+<li onclick="copy(this)" class="rog-icon ic-l ic-dark"><a>rog-icon</a></li>
+
+<li onclick="copy(this)" class="razer-icon ic-l ic-dark"><a>razer-icon</a></li>
+<li onclick="copy(this)" class="waze-icon ic-l ic-dark"><a>waze-icon</a></li>
+<li onclick="copy(this)" class="suzuki-icon ic-l ic-dark"><a>suzuki-icon</a></li>
+<li onclick="copy(this)" class="tiktok-icon ic-l ic-dark"><a>tiktok-icon</a></li>
+<li onclick="copy(this)" class="shazam-icon ic-l ic-dark"><a>shazam-icon</a></li>
+<li onclick="copy(this)" class="honda-icon ic-l ic-dark"><a>honda-icon</a></li>
+<li onclick="copy(this)" class="twitch-icon ic-l ic-dark"><a>twitch-icon</a></li>
+<li onclick="copy(this)" class="toyota-icon ic-l ic-dark"><a>toyota-icon</a></li>
+<li onclick="copy(this)" class="telegram-icon ic-l ic-dark"><a>telegram-icon</a></li>
+
+<li onclick="copy(this)" class="tsnode-icon ic-l ic-dark"><a>tsnode-icon</a></li>
+<li onclick="copy(this)" class="dts-icon ic-l ic-dark"><a>dts-icon</a></li>
+<li onclick="copy(this)" class="ktm-icon ic-l ic-dark"><a>ktm-icon</a></li>
+<li onclick="copy(this)" class="stadia-icon ic-l ic-dark"><a>stadia-icon</a></li>
+<li onclick="copy(this)" class="dolby-icon ic-l ic-dark"><a>dolby-icon</a></li>
+
+
+      ` +
+  "</ul>";
+document.getElementById("konten").innerHTML = content;
 
 function show() {
   document.getElementById("konten1").style.display = "block";
