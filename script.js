ac77513406dda6fb8c675afb2b0cc4f2e5cdf785
Minin <70429604+MininxD@users.noreply.github.com>
2023-09-12 13:32:31 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -80,10 +80,9 @@ setInterval(function () {
 
 ///
 
+setTimeout( function() {
 var ul = document.getElementById("acak");
 for (var i = ul.children.length; i >= 0; i--) {
   ul.appendChild(ul.children[(Math.random() * i) | 0]);
-  setTimeout(function () {
-    ul.style.opacity = "1";
-  }, 1000);
 }
+}, 1000)
